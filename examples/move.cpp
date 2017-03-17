59317ed6d193c5153e7a1badb4bb69377ab071ae
ElenaNazarova <alenan95@mail.ru>
2017-03-17 12:52:25 +0400

    Update move.cpp

--- examples/move.cpp
+++ examples/move.cpp
@@ -1,1 +1,15 @@
+#include <yadisk/client.hpp>
 
+int main () 
+{
+    yandisk::Client client { "token" };
+    url::path from = "/example1";
+    url::path to = "/example2";
+    bool over = true;
+    std::list<std::string> fields {"name"};
+    auto move = client.move(from,to);
+    auto code = move ["code"];
+    auto link = move ["href"];
+    ctdout << code << link;
+      
+}
